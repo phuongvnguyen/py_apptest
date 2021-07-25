d966f8a98241b00278fe31d73e13d039d416ea80
phuongvnguyen <phuongnv@iuj.ac.jp>
2021-07-25 17:37:41 +0700

    fourth commit

--- main_code/modelling.py
+++ main_code/modelling.py
@@ -6,3 +6,6 @@ from sklearn.linear_model import LogisticRegression
 # data loader
 X, y = load_iris(return_X_y=True)
 # print(X)
+clf = LogisticRegression(random_state=0).fit(X, y)
+test = clf.predict(X[:2, :])
+print(test)
