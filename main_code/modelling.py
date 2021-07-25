4b0cf429b9e0dfbb1cd24027753f2e100a39ec2f
phuongvnguyen <phuongnv@iuj.ac.jp>
2021-07-25 17:28:13 +0700

    third commit

--- main_code/modelling.py
+++ main_code/modelling.py
@@ -1,2 +1,8 @@
 import pandas as pd
-import numpy as ny
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+
+# data loader
+X, y = load_iris(return_X_y=True)
+# print(X)
